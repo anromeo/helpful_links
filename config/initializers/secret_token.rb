b5076cbd987b6435bb6fcb7a3238143a595c6e28
anromeo <anthonykzullo@gmail.com>
2013-05-09 11:41:24 -0700

    first commit to helpful_links

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelpfulLinks::Application.config.secret_token = '5ce4dc0f45761f5d83264deac5499eda9d0a78a689e843b7802f9aaf1ebad6f27be90ae34aafde2fefc93678fa68b59c8e3c3267e156c8e5a4b4eed06f844c5f'
